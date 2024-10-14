74d0bb92052f50c70fc81921cab381dab66b6b50
EnzoBelfort <belfortenzo@gmail.com>
2024-10-14 19:26:33 +0000

    estruturas de dados

---
+++ list.py
@@ -0,0 +1,13 @@
+fruits = ['orange', 'apple', 'pear', 'banana']
+
+fruits.count('orange')
+
+fruits.index('orange')
+
+fruits.reverse()
+
+fruits.append('kiwi') #aloca memória automaticamente
+
+fruits.sort()
+
+fruits.pop() #retira o ultimo elemento da lista e retorna ele
