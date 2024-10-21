10ce360ad46c504c4fc726411babc693204c93d0
Enzo Belfort <belfortenzo@gmail.com>
2024-10-21 10:03:09 -0300

    twoSun exercise

--- twoSun.py
+++ twoSun.py
@@ -1,12 +1,6 @@
 def twoSum(nums, target):
-    for i in range(len(nums)):
-        j = i + 1
-        for j in range(len(nums)):
+    for i in range(0,len(nums)):
+        for j in range(i+1, len(nums)):
             if nums[i] + nums[j] == target:
-                print([i, j])
-               
-
-nums = [1, 3, 5]
-target = 4
-
-twoSum(nums, target)
+                return([i, j])
+    return 0
